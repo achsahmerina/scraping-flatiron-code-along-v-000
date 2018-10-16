3fe5a43cebee4e4c5a37e9b52397dbbd788ae7ab
Achsah Abraham <achsahmp@gmail.com>
2018-10-16 18:48:07 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,16 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
+
+  def initialized
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
