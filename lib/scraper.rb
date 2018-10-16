e02db08b0683946f509e54c26f5b968c0a450032
Achsah Abraham <achsahmp@gmail.com>
2018-10-16 19:19:20 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
           course.description = post.css("p").text
         end
     end
-    
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -39,4 +39,5 @@ class Scraper
     end
   end
 
+Scraper.new.print_courses
 end
